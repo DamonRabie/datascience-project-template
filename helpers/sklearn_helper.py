7ee0ed19da885facc8d8d1402541f413e80a7633
Damon <damoun.rabie@dfreight.org>
2023-07-24 16:46:55 +0330

    code: add comments

--- helpers/sklearn_helper.py
+++ helpers/sklearn_helper.py
@@ -8,27 +8,32 @@ from sklearn.metrics import precision_score, recall_score, precision_recall_curv
 from sklearn.model_selection import train_test_split, cross_val_predict
 
 
+# Function to split data into training and testing sets
 def get_train_test(X, y, test_size=0.2, random_state=42):
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 
+# Function to compute precision and recall scores for a given threshold
 def compute_precision_recall(labels, predictions, p=0.5):
     return precision_score(labels, predictions > p), recall_score(labels, predictions > p)
 
 
+# Function to compute F1 score for a given threshold
 def compute_f1_score(labels, predictions, p=0.5):
     return f1_score(labels, predictions > p)
 
 
+# Function to print classification report for a given threshold
 def print_classification_report(labels, predictions, p=0.5, names=['class_0', 'class_1']):
     print(classification_report(y_true=labels, y_pred=predictions >= p, target_names=names))
 
 
+# Function to find the optimal threshold that maximizes the F1 score
 def find_opt_threshold(labels, predictions, metric='f1'):
     best_threshold = 0
     best_score = 0
 
-    # Find the optimum threshold with the highest F1 score
+    # Find the optimum threshold with the highest specified metric score
     for threshold in np.arange(0.1, 1.0, 0.05):
         predictions_thresholded = (predictions >= threshold).astype(int)
         if metric == 'f1':
@@ -38,7 +43,7 @@ def find_opt_threshold(labels, predictions, metric='f1'):
         elif metric == 'precision':
             m = precision_score(labels, predictions_thresholded)
         else:
-            raise "Wrong metric"
+            raise ValueError("Wrong metric")  # Corrected the exception type to ValueError
 
         if m > best_score:
             best_score = m
@@ -47,15 +52,18 @@ def find_opt_threshold(labels, predictions, metric='f1'):
     return best_threshold
 
 
+# Function to compute cross-validated predictions scores
 def compute_cross_val_predict_scores(model, X, y, cv=3):
     if hasattr(model, "decision_function"):
         return cross_val_predict(model, X, y, cv=cv, method="decision_function")
     elif hasattr(model, "predict_proba"):
         return cross_val_predict(model, X, y, cv=cv, method="predict_proba")[:, 1]
     else:
-        raise "Model does not have either decision_function or predict_proba attributes"
+        raise ValueError(
+            "Model does not have either decision_function or predict_proba attributes")  # Corrected the exception type to ValueError
 
 
+# Function to plot various metrics during model training
 def plot_metrics(history, **kwargs):
     metrics = ['loss', 'prc', 'precision', 'recall']
     fig = plt.figure()
@@ -74,6 +82,7 @@ def plot_metrics(history, **kwargs):
         plt.legend()
 
 
+# Function to plot the confusion matrix
 def plot_confusion_matrix(labels, predictions, p=0.5):
     cm = confusion_matrix(labels, predictions > p)
     fig = plt.figure(figsize=(5, 5))
@@ -83,6 +92,7 @@ def plot_confusion_matrix(labels, predictions, p=0.5):
     plt.xlabel('Predicted label')
 
 
+# Function to plot precision-recall curves with different thresholds
 def plot_prc_threshold(name, labels, predictions=None, features=None, model=None, cv=3, **kwargs):
     if model is not None:
         predictions = compute_cross_val_predict_scores(model, features, labels, cv)
@@ -98,6 +108,7 @@ def plot_prc_threshold(name, labels, predictions=None, features=None, model=None
     plt.grid(color='grey', linestyle='--', linewidth=0.5, which="both")
 
 
+# Function to plot precision-recall curve
 def plot_prc(name, labels, predictions=None, features=None, model=None, cv=3, **kwargs):
     if model is not None:
         predictions = compute_cross_val_predict_scores(model, features, labels, cv)
@@ -111,6 +122,7 @@ def plot_prc(name, labels, predictions=None, features=None, model=None, cv=3, **
     plt.grid(color='grey', linestyle='--', linewidth=0.5, which="both")
 
 
+# Function to plot ROC curve
 def plot_roc(name, labels, predictions=None, features=None, model=None, cv=3, random_curve=True, **kwargs):
     if model is not None:
         predictions = compute_cross_val_predict_scores(model, features, labels, cv)
@@ -121,12 +133,14 @@ def plot_roc(name, labels, predictions=None, features=None, model=None, cv=3, ra
     if random_curve:
         plt.plot([0, 1], [0, 1], 'k:', label="Random classifier's ROC curve")
 
+    # Adding an arrow and text to indicate the direction of higher threshold
     plt.gca().add_patch(patches.FancyArrowPatch(
         (0.20, 0.89), (0.07, 0.70),
         connectionstyle="arc3,rad=.4",
         arrowstyle="Simple, tail_width=1.5, head_width=8, head_length=10",
         color="#444444"))
     plt.text(0.12, 0.71, "Higher\nthreshold", color="#333333")
+
     plt.xlabel('False Positive Rate (Fall-Out)')
     plt.ylabel('True Positive Rate (Recall)')
     plt.grid()
